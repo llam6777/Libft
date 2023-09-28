060015252f3c032f5d11d1d88d75118524361612
llam6777 <124125806+llam6777@users.noreply.github.com>
2023-09-28 11:14:21 +0930

    28/09/2023

--- libft.a/ft_strlen.c
+++ libft.a/ft_strlen.c
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
+int	ft_strlen(char* str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	    i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char	*str;
+	int	num;
+
+	str = "Hello World!";
+	num = ft_strlen(str);
+	printf("Number of Characters: %d\n", num);
+	return (0);
+}
