a12e558dbddacd1f2acb4d98a84261f3858ec110
llam6777 <124125806+llam6777@users.noreply.github.com>
2023-11-14 18:15:48 +1030

    Initial Commit

---
+++ libft.a/ft_strlen.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lelam <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 18:13:31 by lelam             #+#    #+#             */
+/*   Updated: 2023/11/14 18:13:31 by lelam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	    i++;
+	return (i);
+}
+
+int	main(void)
+{
+	char	*str;
+	int		num;
+
+	str = "Hello World!";
+	num = ft_strlen(str);
+	printf("Number of Characters: %d\n", num);
+	return (0);
+}
